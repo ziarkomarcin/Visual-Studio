b6d165f6d328ea41fc6b5a949c75e4a6424c9416
ziarkomarcin <61658876+ziarkomarcin@users.noreply.github.com>
2020-07-07 11:10:30 +0200

    main menu v1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace KolkoKrzyzyk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()                                          //MENU PODSTAWOWE GRY
+        {
+            Console.WriteLine();
+            Console.WriteLine("|   KÓŁKO I KRZYŻYK   |");
+            Console.WriteLine(" _____________________");
+            Console.WriteLine("|       START(1)      |");
+            Console.WriteLine("|        EXIT(2)      |");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("By rozpocząć grę musisz wprowadzić liczbę odpowiadającą pozycji w Menu Głównym gry...");
+
+            ChooseMenu();
+        }
+        
+
+
+    }
+}
