5c64c8eb2783b7972cb2f13748a531d063bd3b29
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-07-11 22:04:42 +0200

    logika.class1

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Logika
+{
+    public class Class1
+    {
+        public static void ShowGame(char[,] plansza) //wyświetlenie planszy
+        {
+            for (int wiersz = 0; wiersz < 3; wiersz++)
+            {
+                Console.Write("| ");
+                for (int kolumna = 0; kolumna < 3; kolumna++)
+                {
+                    Console.Write(plansza[wiersz, kolumna]);
+                    Console.Write(" | ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void GamePlayerVsPlayer()
+        {
+
+            char[,] plansza = new char[3, 3];
+            for (int a = 0; a < 3; a++)
+            {
+                for (int b = 0; b < 3; b++)
+                {
+                    plansza[a, b] = '-';        //definicja tablicy
+                }
+            }
+        }
+    }
+}
