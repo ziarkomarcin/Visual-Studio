8140d7626277683be619a86efdf89cc9c03249ff
Marcin Ziarko <61658876+ziarkomarcin@users.noreply.github.com>
2020-07-12 03:58:51 +0200

    logika.class1

--- Class1.cs
+++ Class1.cs
@@ -20,6 +20,8 @@ namespace Logika
 
         public static void GamePlayerVsPlayer()
         {
+            int test = 0;
+            char gracz = 'X';
 
             char[,] plansza = new char[3, 3];
             for (int a = 0; a < 3; a++)
@@ -28,6 +30,78 @@ namespace Logika
                 {
                     plansza[a, b] = '-';        //definicja tablicy
                 }
+            }
+            bool gra = false;
+            while (gra == false)
+            {
+                Console.Clear();
+                Console.WriteLine($"RUNDA GRACZA {gracz}!");
+                Console.WriteLine();
+
+                ShowGame(plansza);
+                int wiersz = 0;
+                int kolumna = 0;
+                Console.WriteLine();
+
+                //obsługa zmiennych
+                do
+                {
+                    Console.Write("Wprowadź wiersz: ");
+                    ConsoleKeyInfo wierszz = Console.ReadKey();
+                    if (wierszz.KeyChar == '0')
+                    {
+                        test = 0;
+                        wiersz = 0;
+                        Console.WriteLine();
+                    }
+                    else if (wierszz.KeyChar == '1')
+                    {
+                        test = 0;
+                        wiersz = 1;
+                        Console.WriteLine();
+                    }
+                    else if (wierszz.KeyChar == '2')
+                    {
+                        test = 0;
+                        wiersz = 2;
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        test++;
+                        Console.WriteLine();
+                    }
+                } while (test > 0);
+
+                do
+                {
+                    Console.Write("Wprowadź kolumnę: ");
+                    ConsoleKeyInfo kolumnaa = Console.ReadKey();
+                    if (kolumnaa.KeyChar == '0')
+                    {
+                        test = 0;
+                        kolumna = 0;
+                        Console.WriteLine();
+                    }
+                    else if (kolumnaa.KeyChar == '1')
+                    {
+                        test = 0;
+                        kolumna = 1;
+                        Console.WriteLine();
+                    }
+                    else if (kolumnaa.KeyChar == '2')
+                    {
+                        test = 0;
+                        kolumna = 2;
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        test++;
+                        Console.WriteLine();
+                    }
+                } while (test > 0);
+                //koniec obsługi zmiennych
             }
         }
     }
